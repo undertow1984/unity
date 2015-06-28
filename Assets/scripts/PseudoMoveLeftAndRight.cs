4c29bbabb47ee2968655c196c8a2e82a685a12fa
jeremy payne <undertow1984@gmail.com>
2015-06-28 14:41:31 -0400

    more gameplay

---
+++ Assets/scripts/PseudoMoveLeftAndRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PseudoMoveLeftAndRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		GetComponent<Renderer>().material.mainTextureOffset += Vector2.right*Input.GetAxis ("Horizontal")* speed * Time.deltaTime;
+	}
+}
